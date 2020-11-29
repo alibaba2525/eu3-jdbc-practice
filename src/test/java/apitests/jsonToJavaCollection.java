a28eafec7eac595fbecc8b3a1e110b2739a0c7ee
Ali Celik <aliihsancelik1@gmail.com>
2020-11-29 20:09:56 +0000

    updated

---
+++ src/test/java/apitests/jsonToJavaCollection.java
@@ -0,0 +1,37 @@
+package apitests;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.*;
+
+public class jsonToJavaCollection {  // Di-serialization way(4th way) of dealing with json response:
+    //But for this way, for converting json to java collection we also need to put Gson or Jackson dependency to pom xml!!
+
+    @BeforeClass
+    public void beforeClass(){
+        baseURI = ConfigurationReader.get("spartan_api_url");
+    }
+
+    @Test
+    public void spartanToMap(){
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id", 15)
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(response.statusCode(),200);
+
+        //we will convert json response to java map
+        Map<String,Object> jsonDataMap = response.body().as(Map.class);
+        System.out.println(jsonDataMap);
+
+    }
+}
